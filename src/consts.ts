55e99e724d85307a66de30f529e1a8d5527d70a6
Liam Connors <connorsl@tcd.ie>
2026-07-09 11:09:57 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'morningcashee';
+export const SITE_DESCRIPTION = 'morningcashee is a blog and portfolio site by Liam Connors, a technical writer and docs engineer.';
